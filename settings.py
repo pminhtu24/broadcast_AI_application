d20ef3846fb66fcdfcf059a88750a3b1a3441f65
pminhtu24 <pminhtu24@gmail.com>
2026-04-01 16:16:34 +0700

    small fix

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     INFINITY_URL: str
     VIETTEL_BASE_URL: str
     VIETTEL_API_KEY: SecretStr
-    VIETTEL_MODEL: str = "accounts/fireworks/models/gpt-oss-120b"
+    VIETTEL_MODEL: str
 
     class Config:
         env_file = ".env"
