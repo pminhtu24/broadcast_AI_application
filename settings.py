3bb79d15e25e359d3add1a4902c65990048bd67c
pminhtu24 <pminhtu24@gmail.com>
2026-04-01 16:09:05 +0700

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    NEO4J_URI: str
+    NEO4J_USERNAME: str
+    NEO4J_PASSWORD: SecretStr
+    INFINITY_URL: str
+    VIETTEL_BASE_URL: str
+    VIETTEL_API_KEY: SecretStr
+    VIETTEL_MODEL: str = "accounts/fireworks/models/gpt-oss-120b"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings() #type: ignore
